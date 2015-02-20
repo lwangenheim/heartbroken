570cb26ed9c852f9a7931f3b7abeb510df6a81b5
sambenson <sam@futurebuilding.co.uk>
2015-02-20 15:58:16 +0000

    Removed support for other browsers

--- content-script.js
+++ content-script.js
@@ -1,5 +1,5 @@
 var css = '.favorite-image.favorited, .favorite-image:active { color: #85BF25 !important; }',
-    head = document.head || document.getElementsByTagName('head')[0],
+    head = document.head,
     style = document.createElement('style');
 
 style.appendChild(document.createTextNode(css));
